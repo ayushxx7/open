310359bcf51f04e5b5c2bfe8d63fee6d2a74f8df
ayushxx7 <ayushxx7@gmail.com>
2018-12-24 07:35:22 +0530

    cleaned out, jinja'd up

---
+++ run.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return(render_template('index.html'))
+
+@app.route('/index.html')
+def index2():
+	return(render_template('index.html'))
+
+@app.route('/post.html')
+def post():
+	return(render_template('post.html'))
+
+@app.route('/about.html')
+def about():
+	return(render_template('about.html'))
+
+@app.route('/contact.html')
+def contact():
+	return(render_template('contact.html'))
+
+if (__name__ == "__main__"):
+	app.run(debug=True)
